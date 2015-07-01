1f9dc50a6af376b5642aed5a21caab4c36fd894c
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-30 23:48:48 -0700

    add collection

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -5,10 +5,10 @@ var postsData = [ {
   {
     title: 'Meteor',
     url: 'http://meteor.com'
-   }, 
-   {
+  }, 
+  { title: 'The Meteor Book',
     url: 'http://themeteorbook.com'
-  }
+  },
 ];
 Template.postsList.helpers({
   posts: postsData
